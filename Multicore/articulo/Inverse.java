5a6cd8b775a1d5bc18db2a3027842953f1417959
Diego <dgalindez1@gmail.com>
2015-10-31 10:22:43 -0600

    Sort of parallel java

--- Multicore/articulo/Inverse.java
+++ Multicore/articulo/Inverse.java
@@ -3,11 +3,11 @@
  *  A01370815 Diego Galindez Barreda
  *  A01165988 Eduardo Azuri Gaytan Martinez 
  */
-import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.stream.*;
 
 public class Inverse {
-    public static int MAXN = 1000;
+    public static int MAXN = 4;//1000;
 
     // Sequential code
     public static double[][] invert(double matrix[][]) {
@@ -15,81 +15,82 @@ public class Inverse {
             matrix[i][MAXN + i] = 1;
         }
 
-        gaussJordan(matrix, 0);
-
-        return matrix;
+        return gaussJordan(matrix);
     }
 
-    public static double[][] gaussJordan(double matrix[][], int i) {
-        if (i == matrix.length) {
-            return matrix;
-        }
+    public static double[][] gaussJordan(double matrix[][]) {
+        int i, j, k;
+        double val, scale;
+        double[] irow, jrow;
 
-        double[] irow = new double[MAXN * 2];
-        irow = Arrays.copyOf(matrix[0], MAXN * 2);
-        double val = irow[i];
-        for (int j = 0; j < MAXN * 2; j++) {
-            irow[j] /= val;
-        }
-
-        for (int j = 1; j < MAXN; j++) {
-            double[] jrow = matrix[j];
-            double scale = matrix[j][i];
-            for (int k = 0; k < MAXN * 2; k++) {
-                jrow[k] -= (irow[k] * scale);
+        for (i = 0; i < MAXN; i++) {
+            irow = Arrays.copyOf(matrix[0], MAXN * 2);
+            val = irow[i];
+            for (j = 0; j < MAXN * 2; j++) {
+                irow[j] /= val;
             }
-        }
-        for (int j = 0; j < MAXN - 1; j++) {
-            matrix[j] = matrix[j + 1];
-        }
-        matrix[MAXN - 1] = new double[MAXN * 2];
-        matrix[MAXN - 1] = Arrays.copyOf(irow, MAXN * 2);
 
-        return gaussJordan(matrix, i + 1);
+            for (j = 1; j < MAXN; j++) {
+                jrow = matrix[j];
+                scale = matrix[j][i];
+                for (k = 0; k < MAXN * 2; k++) {
+                    jrow[k] -= (irow[k] * scale);
+                }
+            }
+            for (j = 0; j < MAXN - 1; j++) {
+                matrix[j] = matrix[j + 1];
+            }
+            matrix[MAXN - 1] = Arrays.copyOf(irow, MAXN * 2);
+        }
+        return matrix;
     }
 
     // Parallel code
     public static double[][] parallelInvert(double matrix[][]) {
-        for (int i = 0; i < MAXN; i++) {
-            matrix[i][MAXN + i] = 1;
-        }
+        IntStream
+            .range(0, MAXN)
+            .parallel()
+            .forEach(i -> {
+                matrix[i][MAXN + i] = 1;
+            });
 
-        parallelGaussJordan(matrix, 0);
+        parallelGaussJordan(matrix);
 
         return matrix;
     }
 
-    public static double[][] parallelGaussJordan(double matrix[][], int i) {
-        if (i == matrix.length) {
-            return matrix;
-        }
+    public static double[][] parallelGaussJordan(double matrix[][]) {
+        double val, scale;
+        double[] irow, jrow;
 
-        double[] irow = new double[MAXN * 2];
-        irow = Arrays.copyOf(matrix[0], MAXN * 2);
-        double val = irow[i];
-        for (int j = 0; j < MAXN * 2; j++) {
-            irow[j] /= val;
-        }
+        IntStream.range(0, MAXN).forEach(i ->
+            irow = Arrays.copyOf(matrix[0], MAXN * 2);
+            val = irow[i];
 
-        for (int j = 1; j < MAXN; j++) {
-            double[] jrow = matrix[j];
-            double scale = matrix[j][i];
-            for (int k = 0; k < MAXN * 2; k++) {
-                jrow[k] -= (irow[k] * scale);
+            IntStream.range(0, MAXN * 2).forEach(j ->
+                irow[j] /= val;
+            );
+
+            IntStream.range(1, MAXN).forEach(j -> {
+                jrow = matrix[j];
+                scale = matrix[j][i];
+                for (int k = 0; k < MAXN * 2; k++) {
+                    jrow[k] -= (irow[k] * scale);
+                }
+            });
+
+            for (int j = 0; j < MAXN - 1; j++) {
+                matrix[j] = matrix[j + 1];
             }
-        }
-        for (int j = 0; j < MAXN - 1; j++) {
-            matrix[j] = matrix[j + 1];
-        }
-        matrix[MAXN - 1] = new double[MAXN * 2];
-        matrix[MAXN - 1] = Arrays.copyOf(irow, MAXN * 2);
 
-        return parallelGaussJordan(matrix, i + 1);
+            matrix[MAXN - 1] = Arrays.copyOf(irow, MAXN * 2);
+        });
+        return matrix;
     }
 
     public static void printMatrix(double[][] matrix) {
-        for (int i = 0; i < matrix.length; i++) {
-            for (int j = MAXN; j < matrix[0].length; j++) {
+        for (int i = 0; i < MAXN; i++) {
+            for (int j = MAXN; j < MAXN * 2; j++) {
                 System.out.print(matrix[i][j] + ", ");
             }
             System.out.println();
@@ -98,8 +99,6 @@ public class Inverse {
     }
 
     public static void benchmark(String title, double[][] matrix, boolean sequential) {
-        double[][] inverse;
-
         System.out.println(title);
         //printMatrix(matrix);
 
@@ -112,31 +111,40 @@ public class Inverse {
         long end = System.currentTimeMillis();
         double time = (end - start) / 1000.0;
 
-        //printMatrix(matrix);
+        printMatrix(matrix);
         System.out.printf("Time: %.2f%n", time);
     }
 
     public static void main(String... args) {
+        /*
+        double randomNum;
+        double[][] sequential = new double[MAXN][MAXN * 2];
+        double[][] parallel = new double[MAXN][MAXN * 2];
+        for (int i = 0; i < MAXN; i++) {
+            for (int j = 0; j < MAXN; j++) {
+                randomNum = 1.0 + (Math.random() * MAXN);
+                sequential[i][j] = randomNum;
+                parallel[i][j] = randomNum;
+            }
+        }
+        //*/
         //*
-           double randomNum;
-           double[][] matrix = new double[MAXN][MAXN * 2];
-           for (int i = 0; i < MAXN; i++) {
-               for (int j = 0; j < MAXN; j++) {
-                   randomNum = 1.0 + (Math.random() * MAXN);
-                   matrix[i][j] = randomNum;
-               }
-           }
-           //*/
-           /*
-        double[][] matrix = {
+        double[][] sequential = {
             {1, 1, 2, 1, 0, 0, 0, 0},
             {1, 2, 1, 2, 0, 0, 0, 0},
             {1, 1, 1, 3, 0, 0, 0, 0},
             {2, 3, 1, 3, 0, 0, 0, 0}
         };
-           */
 
-        benchmark("Sequential", Arrays.copyOf(matrix, MAXN), true);
-        benchmark("Concurrent", matrix, false);
+        double[][] parallel = {
+            {1, 1, 2, 1, 0, 0, 0, 0},
+            {1, 2, 1, 2, 0, 0, 0, 0},
+            {1, 1, 1, 3, 0, 0, 0, 0},
+            {2, 3, 1, 3, 0, 0, 0, 0}
+        };
+        //*/
+
+        benchmark("Sequential", sequential, true);
+        benchmark("Concurrent", parallel, false);
     }
 }
